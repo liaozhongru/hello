5e880d8fb1e63a220785bebfc11d494abb6fac22
liaozhongru <liao.zhongru@qq.com>
2015-08-11 14:24:30 +0800

    hello
    C language

---
+++ hello/hello/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  hello
+//
+//  Created by 廖忠儒 on 15-8-11.
+//  Copyright (c) 2015年 廖忠儒. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
